e35e75a148c061d50a6015ce72ebac209893c73f
volbil <volbilnexus@gmail.com>
2021-09-30 23:39:24 +0900

    Updated logo

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index():
 def app_json():
     return {
         "name": "Callback Test",
-        "icon": "https://source.boringavatars.com/beam/120/codepillow?colors=264653,2a9d8f,e9c46a,f4a261,e76f51"
+        "icon": "https://callback.codepillow.io/static/codepillow.png"
     }
 
 @app.route("/call/<string:session>", methods=["POST"])
